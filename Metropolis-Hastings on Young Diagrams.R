7e6d845ba09e3b3145766640de276307215a04a7
Ruheyan Nuermaimaiti <66019248+Ruheyan@users.noreply.github.com>
2022-09-27 15:33:23 +0100

    update the main loop

--- Metropolis-Hastings on Young Diagrams.R
+++ Metropolis-Hastings on Young Diagrams.R
@@ -1,13 +1,13 @@
-#some settings                          #integer of partitions
-z<-0.8    # 0.9        #P(n-1)/P(n)                     #the value of z
-N<-10000  #1000   #400000             #iterations of MCMC
+#MCMC on Young Diagrams                          #integer of partitions
+library(partitions)
+set.seed(6) #6 gives good histogram
 
-#starting point  
-lambda.start<-c(5,4,3,1,1,1)
+z<-0.85               #or P(n-1)/P(n) 
 
+N<-500000            #iterations of MCMC
+lambda.start<-c(1)   #initial state (starting point)
 
 #some functions 
-# in this function how do I set 
 add_y <- function(y, i) {
   sigma <- length(unique(y))
   if (i > sigma+1) {
@@ -23,21 +23,23 @@ add_y <- function(y, i) {
   return(list(valid=TRUE, y=y))
 } 
 
-
 remove_y <- function(y, i) {
   sigma <- length(unique(y))
   pos <- max(which(y==unique(y)[i]))
-  if (i >= sigma){
+  if (i > sigma){
     return(list(valid=FALSE))
   } else if (y[1]==0){
     return(list(valid=FALSE))}
   if (y[1]!=0){
     y[pos]<-y[pos]-1
-    y <- y}
-    if (y[1]==0){ 
-      y<-y
-    } else {y= y[-which(y==0)] }
-    return(list(valid=TRUE, y=y))
+    y=y
+  }
+  if (y[1]!=0){
+    y <-y[y!=0]
+  }else{ 
+    y<-y
+  } 
+  return(list(valid=TRUE, y=y))
 }
 
 #MCMC method 
@@ -45,61 +47,66 @@ lambda<-vector("list",N)
 lambda[[1]]<-lambda.start
 lambda.abs<-matrix(0,nrow =N , ncol=1 )
 lambda.abs[1,]<-sum(lambda.start)
+lambda.parts<-matrix(0,nrow =N , ncol=1 )
+lambda.parts[1,]<-length(lambda.start)
 rho<-c()
 for (t in 2:N){
-  sigma<-length(unique(lambda[[t-1]]))
-  b<-sample(c(-1,1),1)
-  if (b==1){
-    i<-sample(sigma+1,1)
-    rho<-add_y(lambda[[t-1]],i)$y
-    if (add_y(lambda[[t-1]],i)$valid==TRUE){
-      #accept with prob. min(alpha,1)  
-      if (sum(lambda[[t-1]])>0) {
-        alpha<-z*(sigma+1)/sigma
-      } else {
-        alpha <- z / 2
-      }
+  s <- lambda[[t-1]]
+  sigma<-length(unique(s))
+  if (sum(s)>0){
+    b<-sample(c(-1,1),1)
+    if (b==1){
+      i<-sample(sigma+1,1)
+      rho<-add_y(s,i)$y
+      alpha<-z*(sigma+1)/sigma
       u<-runif(1,0,1)
       if (u<=alpha){
         lambda[[t]]<-rho
       }else{ #stay with prob. 1-alpha
-        lambda[[t]]<-lambda[[t-1]]
+        lambda[[t]]<-s
       }
-    }else{ #stay if after adding rho isn't valid
-      lambda[[t]]<-lambda[[t-1]]
-    }
-    
-  }else{     #when b==-1
-    if(sum(lambda[[t-1]])>0){
+    }else{     #when b==-1
       i<-sample(sigma,1)
-      rho<-remove_y(lambda[[t-1]],i)$y
-      if (remove_y(lambda[[t-1]],i)$valid==TRUE){
-        #accept with prob. min(alpha,1)
-        alpha<-z^(-1)*sigma/(sigma+1)
-        u<-runif(1,0,1)
-        if(u<=alpha){
-          lambda[[t]]<-rho
-        }else{
-          lambda[[t]]<-lambda[[t-1]]
-        } 
-      }else{ #stay if after removing rho isn't valid
-        lambda[[t]]<-lambda[[t-1]]
+      rho<-remove_y(s,i)$y
+      #accept with prob. min(alpha,1)
+      alpha<-z^(-1)*sigma/(sigma+1)
+      u<-runif(1,0,1)
+      if(u<=alpha){
+        lambda[[t]]<-rho
+      }else{
+        lambda[[t]]<-s
       }
-    }else{ #if the partition goes 0 force it to be 1
-      alpha <- z / 2
-      u <- runif(1, 0, 1)
-      if (u <= alpha) {
-        lambda[[t]]=1
-      } else {
-        lambda[[t]]<-lambda[[t-1]]
+    }
+  } else {
+    if (sum(s==0)){
+      b<-sample(c(0,1),1)
+      if (b==1){
+        rho=c(1)
+        alpha<-1
+        u<-runif(1,0,1)
+        if (u<=alpha){
+          lambda[[t]]<-rho
+        }else{ #stay with prob. 1-alpha
+          lambda[[t]]<-s
+        }
+      } else{ # if b=0
+        rho=c(0) 
+        alpha<-1
+        u<-runif(1,0,1)
+        if (u<=alpha){
+          lambda[[t]]<-rho
+        }else{ #stay with prob. 1-alpha
+          lambda[[t]]<-s
+        }
       }
     }
   }
   lambda.abs[t]<-sum(lambda[[t]])
+  lambda.parts[t]<-length(lambda[[t]])
 }
 
 mean(lambda.abs)
-plot.ts(lambda.abs)
+plot.ts(lambda.abs,ylim=c(-20,200), ylab=expression(abs(lambda)))
 
 hist(lambda.abs, probability = TRUE, 
      col="grey",
@@ -111,18 +118,17 @@ ll <- seq(0, max(lambda.abs)+1, by=1)
 e <- numeric(length(ll))
 for (i in 1:length(ll)) {
   l <- ll[i]
-  e[i] <- z^l 
+  e[i] <- z^l * P(l)
 }
 lines(ll, e/sum(e), col="red", lwd=1.5)
-acf(lambda.abs)
 
-
+acf(lambda.abs, main="")
 
 
 
 pdf("MHYDTs.pdf",width = 5,height= 3.5)
 par(mar = c(4, 4, 2, 2),xaxs= "r",yaxs  = "r",cex.axis = 1,cex.lab  = 1)
-plot.ts(lambda.abs)
+plot.ts(lambda.abs,ylim=c(-20,200), ylab=expression(abs(lambda)))
 dev.off()
 
 pdf("MHYDHistogram.pdf",width = 5,height= 3.5)
@@ -144,15 +150,3 @@ dev.off()
 
 (mean(lambda.abs))
 (var(lambda.abs))
-
-
-
-
-
-
-
-
-
-
-
-
